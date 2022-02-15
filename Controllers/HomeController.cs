4322ba5040522544f6a6fa04ef6f44b0e311e68b
jhadley1922 <90360017+jhadley1922@users.noreply.github.com>
2022-02-15 14:07:43 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
